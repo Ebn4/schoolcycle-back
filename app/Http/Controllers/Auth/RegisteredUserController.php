9dd9bb42a56def6ef3e45a73aa4ae094348fec17
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-25 20:06:43 +0200

    Modification des routes et des controllers pour l'authentification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -22,12 +22,18 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'phone' => ['required', 'string', 'max:10', 'unique:'.User::class],
+            'job' => ['required', 'string', 'max:255'],
+            'avatar' => ['nullable', 'image', 'max:2048'], // Optional avatar field
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
+            'job' => $request->job,
+            'avatar' => $request->file('avatar') ? $request->file('avatar')->store('avatars', 'public') : null, // Store avatar if provided
             'password' => Hash::make($request->string('password')),
         ]);
 
